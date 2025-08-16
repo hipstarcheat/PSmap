105d6fee4dac8ceabfb2d6725bc9c7ab7328cda8
Вольфрам <kiryushinrus@gmail.com.com>
2025-08-16 07:47:17 +0300

    Обнова

--- public/script.js
+++ public/script.js
@@ -43,21 +43,32 @@ map.on('click', async function(e) {
 });
 
 
-// Загружаем зоны и рисуем полигоны
-fetch('/api/zones')
-    .then(res => res.json())
-    .then(zones => {
-        zones.forEach(zone => {
-            const polygon = L.polygon(zone.coords, {
-                color: 'red',         // цвет линии
-                weight: 2,            // толщина линии
-                fillColor: 'red',     // цвет заливки
-                fillOpacity: 0.3      // прозрачность заливки
-            }).addTo(map);
+// Загружаем районы СПб
+fetch('spb_districts.geojson')
+  .then(res => res.json())
+  .then(data => {
+    L.geoJSON(data, {
+      style: (feature) => ({
+        color: getRandomColor(), // случайный цвет для наглядности
+        weight: 2,
+        fillOpacity: 0.3
+      }),
+      onEachFeature: (feature, layer) => {
+        layer.bindPopup(feature.properties.name);
+      }
+    }).addTo(map);
+  });
 
-            polygon.bindPopup(`${zone.name} (значение: ${zone.value})`);
-        });
-    });
+// Функция для случайного цвета
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 
 
 
